8a34e09525cb483b2437b5b2d257eb771e4fabb8
Toughee <Jimernez.io@gmail.com>
2022-04-04 22:25:55 -0500

    Rainnin/004/UI_Update_02 - Updated UI design for app - Updated logo, README file and favicon for app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import './App.css'
 
+import {
+    BsFillCloudRainFill,
+    BsFillCloudSnowFill,
+    BsFillCloudyFill,
+    BsSunFill,
+    BsMoonFill,
+} from 'react-icons/bs'
 import { BiRefresh, BiSearchAlt } from 'react-icons/bi'
 
 function App() {
@@ -8,14 +15,41 @@ function App() {
             <header>
                 <h1 className="title">Rainnin</h1>
             </header>
-
+            {/*
             <button className="ActionStyle">
                 <BiRefresh />
-            </button>
+            </button>*/}
             <input className="SearchBar" placeholder="enter a city here" />
             <button className="ActionStyle">
-                <BiSearchAlt />
+                <BiSearchAlt /> Search
             </button>
+
+            <main className="DataTab">
+                <span className="WeatherTitle">
+                    The weather for London, United Kingdom is:{' '}
+                </span>
+                <br />
+                <br />
+                <br />
+                <span className="WeatherIcon">
+                    <BsFillCloudRainFill />
+                </span>
+                <span className="WeatherIcon">
+                    <BsFillCloudSnowFill />
+                </span>
+                <span className="WeatherIcon">
+                    <BsFillCloudyFill />{' '}
+                </span>
+                <span className="WeatherIcon">
+                    <BsSunFill />{' '}
+                </span>
+                <span className="WeatherIcon">
+                    <BsMoonFill />
+                </span>
+            </main>
+            <br></br>
+            <br></br>
+            <br></br>
         </div>
     )
 }
